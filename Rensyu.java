69dd9de88aaa16839057ddbf540ef2679a203cc9
unknown <183118@lab-ocs.local>
2019-01-23 08:53:54 +0900

    Rensyu.java

--- Rensyu.java
+++ Rensyu.java
@@ -1,7 +1,12 @@
 
 public class Rensyu{
 	public static void main(String[] args) {
-		System.out.println("Hello Would");
+		outPut();
+	}
+
+
+	public void outPut() {
+		System.out.println("アウトプットメソッドを呼び出しました");
 	}
 }
 
