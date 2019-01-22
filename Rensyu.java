0bb5bb15e1c8bc6ea9504cb3ab96b3f2aa3ddfae
unknown <183118@lab-ocs.local>
2019-01-22 10:30:35 +0900

    Rensyu.java

--- Rensyu.java
+++ Rensyu.java
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 public class Rensyu{
 	public static void main(String[] args) {
 		System.out.println("Hello Would");
 	}
 }
-=======
 
->>>>>>> 529d7467ad668910ac12c06c8185fe241e738fcd
