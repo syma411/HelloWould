58059794874af6fed63ff013ecbb3fe23f08d931
unknown <183118@lab-ocs.local>
2019-01-23 09:12:04 +0900

    Rensyu.java

--- Rensyu.java
+++ Rensyu.java
@@ -1,12 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Rensyu{
 	public static void main(String[] args) {
-		outPut();
+		String inputChar;
+		inputChar = input();
+		output(inputChar);
 	}
 
 
-	public void outPut() {
+	public static String input() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String a = null;
+		try{
+			a = br.readLine();
+			return a;
+		}catch(IOException e) {
+			System.out.println(e);
+			return a;
+		}
+	}
+
+	public static void output(String x) {
 		System.out.println("アウトプットメソッドを呼び出しました");
 	}
 }
+
 
